6329f4cd10bd9d80b1ab503241ec4f8b6ae0ce9b
gvarshithreddy <gvarshithreddy8@gmail.com>
2024-05-17 11:09:52 +0530

    added problem 6

---
+++ problem_6/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+function App() {
+  useEffect(() => {}, []);
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Counter key={1} index={1}></Counter>
+      <Counter key={2} index={2}></Counter>
+    </div>
+  );
+}
+
+export default App;
+
+// fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata", {
+//       method: "GET",
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//       });
---
+++ problem_6/src/components/Counter.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+function Counter(props) {
+  const [count, setCount] = useState(0);
+
+  // Simulates fetching data on mount (componentDidMount)
+  useEffect(() => {
+    console.log("Component mounted (useEffect)");
+    const fetchData = async () => {
+      fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata", {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log("Data fetched");
+    };
+    fetchData();
+  }, []); // Empty dependency array: runs only once on mount
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  // Simulates cleanup on unmount (componentWillUnmount)
+  useEffect(() => {
+    console.log("Adding cleanup logic (useEffect)");
+    const isIdle = setInterval(() => console.log("Component is Idle"), 1000);
+    return () => clearInterval(isIdle); // Cleanup function for unmount
+  }, []); // Empty dependency array: runs only once on mount and cleanup on unmount
+
+  console.log(`Component ${props.index} rendered (render)`); // Simulates render method
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={increment}>Click me</button>
+    </div>
+  );
+}
+
+export default Counter;
